9c5a2994a0042e5f419d08ce4755c8ce6c4e2de7
Aetrix27 <davedogg323@gmail.com>
2021-06-11 10:43:18 -0700

    weather description and date are retrieved

--- routes/moods.js
+++ routes/moods.js
@@ -1,42 +1,46 @@
 // PET ROUTES
-var weather = require('openweather-apis');
+let request = require('request')
 var weatherArr = []
+
 
 module.exports = (app) => {
 
     // INDEX PET => index.js
     // NEW PET
-    app.get('/moods-show', (req, res) => {
+    app.get('/moods-create', (req, res) => {
         
-      weather.setLang('it');
-
-      // set city by name
-      weather.setCity('Los Angeles');
-
-      // 'metric'  'internal'  'imperial'
-      weather.setUnits('metric');
-
-      // check http://openweathermap.org/appid#get for get the APPID
-      weather.setAPPID('0ac789480808bd58c4f6f76ca2a66e11');
-        
-      // get the Temperature  
-      weather.getTemperature(function(err, temp){
-        console.log(temp);
-      });
-        res.render('moods-show');
+     
+        res.render('moods-create');
     });
 
     app.get('/', (req, res) => {
-      res.render('moods-show');
+      res.render('moods-index');
     });
   
     // CREATE PET
-    app.post('/moods-show', (req, res) => {
-      // get the Temperature  
-      weather.getTemperature(function(err, temp){
-        weatherArr.append(temp)
-        console.log(weatherArr)
+    app.post('/moods-create', (req, res) => {
+
+      let url = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.0953&lon=118.1270&exclude={part}&appid=0ac789480808bd58c4f6f76ca2a66e11'
+
+      request(url, function (err, response, body) {
+        if(err){
+          console.log('error:', error);
+        } else {
+          let weather = JSON.parse(body)
+          
+          days = weather.daily
+          for (i=0; i<days.length; i++){
+            console.log(days[i]["dt"])
+            console.log(days[i]['weather'][0]['description'])
+            const dateObject = new Date(days[i]["dt"])
+            const humanDateFormat = dateObject.toLocaleString() 
+            console.log(dateObject.toLocaleString("en-US", {timeZoneName: "short"}))
+          }
+        }
       });
+  
+      res.redirect(`/`);
+
 
     })
 }
