07ea6bc9a3a3e824850c4fc904bcd7dad86c50c3
Aetrix27 <davedogg323@gmail.com>
2021-06-12 11:15:51 -0700

    Program is functional

--- routes/moods.js
+++ routes/moods.js
@@ -1,15 +1,40 @@
 // PET ROUTES
 let request = require('request')
-var moodArr = []
-var weatherArr = []
+
 var weatherMoodArr = []
 var selectedDate = ""
+var mood = ""
+var dayWeather = ""
 
 
 module.exports = (app) => {
     app.get('/moods-create', (req, res) => {  
-     
-        res.render('moods-create', {weatherMoodArr});
+        let url = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.0953&lon=118.1270&exclude={part}&appid=0ac789480808bd58c4f6f76ca2a66e11'
+
+        request(url, function (err, response, body) {
+          if(err){
+            console.log('error:', error);
+          } else {
+            var weather = JSON.parse(body)
+            
+            days = weather.daily
+            for (i=0; i<days.length; i++){
+              console.log(days[i]['weather'][0]['description'])
+              var dateObject = new Date(days[i]["dt"]*1000)
+              mnth=dateObject.toLocaleString("en-US", {month: "numeric"})
+              dy=dateObject.toLocaleString("en-US", {day: "numeric"}) 
+              yr=dateObject.toLocaleString("en-US", {year: "numeric"})
+              console.log(mnth+"/"+dy+"/"+yr)
+              var formattedDate=mnth+"/"+dy+"/"+yr
+              if(selectedDate == formattedDate){
+                console.log("THEY ARE THE SAME")
+                dayWeather = days[i]['weather'][0]['description']
+                res.render('moods-create', {dayWeather});
+              }
+            }
+
+          }
+        });
     });
 
     app.get('/', (req, res) => {
@@ -25,33 +50,9 @@ module.exports = (app) => {
 
     // CREATE PET
     app.post('/moods-create', (req, res) => {
+      mood = req.body.moods
+      weatherMoodArr.push([mood, dayWeather, selectedDate])
 
-      let url = 'https://api.openweathermap.org/data/2.5/onecall?lat=34.0953&lon=118.1270&exclude={part}&appid=0ac789480808bd58c4f6f76ca2a66e11'
-
-      request(url, function (err, response, body) {
-        if(err){
-          console.log('error:', error);
-        } else {
-          var weather = JSON.parse(body)
-          
-          days = weather.daily
-          for (i=0; i<days.length; i++){
-            console.log(days[i]['weather'][0]['description'])
-            var dateObject = new Date(days[i]["dt"]*1000)
-            mnth=dateObject.toLocaleString("en-US", {month: "numeric"})
-            dy=dateObject.toLocaleString("en-US", {day: "numeric"}) 
-            yr=dateObject.toLocaleString("en-US", {year: "numeric"})
-            console.log(mnth+"/"+dy+"/"+yr)
-            var formattedDate=mnth+"/"+dy+"/"+yr
-            if(selectedDate == formattedDate){
-              console.log("THEY ARE THE SAME")
-            }
-          }
-
-        }
-      });
-
-      weatherMoodArr.push(23432)
       //res.send()
       res.redirect(`/`);
 
